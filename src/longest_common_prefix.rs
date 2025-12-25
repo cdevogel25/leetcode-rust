741c6e38662e09a16931cd306d3295ca11fc0c00
Calvin D. <cdevogel25@gmail.com>
2025-12-24 22:57:46 -0600

    lcp

--- src/longest_common_prefix.rs
+++ src/longest_common_prefix.rs
@@ -2,13 +2,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        let mut longest_prefix: String = "".to_string();
-        let first = strs.first().unwrap().to_string();
-        for entry in strs {
-            match entry.contains(first) {
-                
-            }
+        let mut first = strs.first().unwrap().to_string();
+        let mut _t = 0;
+        
+        loop {
+            _t = strs.iter().filter(|s| s.starts_with(&first)).count();
+            if _t != strs.len() { first.pop() } else { return first };
         }
-        longest_prefix
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_longest_common_prefix() {
+        let strs: Vec<String> = vec!["flower".to_string(),"flow".to_string(),"flight".to_string()];
+
+        assert_eq!(Solution::longest_common_prefix(strs), "fl".to_string());
     }
 }
