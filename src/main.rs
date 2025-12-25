3b228254b5dce9f0ba3cabcd0487bce4c335057f
Calvin D. <cdevogel25@gmail.com>
2025-12-25 09:42:53 -0600

    how to parenthesis??

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod add_two_numbers;
 pub mod is_palindrome;
 pub mod roman_to_int;
 pub mod longest_common_prefix;
+pub mod valid_parentheses;
 
 fn main() {
     // assert_eq!(add_two_numbers::Solution::add_two_numbers(l1, l2));
---
+++ src/valid_parentheses.rs
@@ -0,0 +1,26 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = "".to_string();
+
+        s.chars().for_each(|x| match x {
+            '(' | '{' | '[' => { stack.push(x); }
+            ')' | '}' | ']' => { if stack.ends_with( ')' | '{' | '[' ) {
+                
+            }}
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_is_valid() {
+        let t: String = "(){}[]".to_string();
+
+        assert!(Solution::is_valid(t));
+    }
+}
