c8546655b04a31f8aeaba5e6913e5d1c35cc508b
Calvin D. <cdevogel25@gmail.com>
2025-12-26 07:37:44 -0600

    they're calling me Edge Case Eric

--- src/valid_parentheses.rs
+++ src/valid_parentheses.rs
@@ -3,15 +3,18 @@ pub struct Solution;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut stack = "".to_string();
+        let mut sv = true;
+
+        if matches!(s.chars().next().unwrap(), ')' | ']' | '}') { return false }
 
         s.chars().for_each(|x| match x {
             '(' | '{' | '[' => { stack.push(x); }
-            ')' => { if stack.ends_with('(') { stack.pop(); continue; } else { return false }}
-            ']' => { if stack.ends_with('[') { stack.pop(); continue; } else { return false }}
-            '}' => { if stack.ends_with('{') { stack.pop(); continue; } else { return false }}
-            _ => return false
-        })
-        true
+            ')' => { if stack.ends_with('(') { stack.pop(); } else { sv = false; return }}
+            ']' => { if stack.ends_with('[') { stack.pop(); } else { sv = false; return }}
+            '}' => { if stack.ends_with('{') { stack.pop(); } else { sv = false; return }}
+            _ => return
+        });
+        return stack == "".to_string() && sv
     }
 }
 
@@ -22,7 +25,13 @@ mod test {
     #[test]
     fn test_is_valid() {
         let t: String = "(){}[]".to_string();
+        let t2: String = "([)]".to_string();
+        let t3: String = "]".to_string();
+        let t4: String = "(])".to_string();
 
         assert!(Solution::is_valid(t));
+        assert!(!Solution::is_valid(t2));
+        assert!(!Solution::is_valid(t3));
+        assert!(!Solution::is_valid(t4));
     }
 }
